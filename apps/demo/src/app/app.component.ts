ca2c6b7c57a051a016687b8421c5185d89f4bbea
Caleb Ukle <caleb.ukle+github@pm.me>
2023-01-25 09:42:50 -0600

    Initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cy-ct-no-exit-bug-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
